ed07421928e53a2958279f46e56e9f0db22d3851
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-07-23 15:37:20 +0900

    밥알 개수와 물방울 개수 연산으로 레벨 계산 구현

--- damagochiProject/Grow/GrowingViewController.swift
+++ damagochiProject/Grow/GrowingViewController.swift
@@ -14,9 +14,16 @@ class GrowingViewController: UIViewController {
     
     @IBOutlet weak var drinkLabel: UILabel!
     
+    
+    @IBOutlet weak var levelText: UILabel!
     @IBOutlet weak var foodTextField: UITextField!
+    
+    @IBOutlet weak var drinkTextField: UITextField!
+    
+    
     var foodCount = 0
-    var driknkCount = 0
+    var driknCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         foodTextFieldDesign()
@@ -27,6 +34,38 @@ class GrowingViewController: UIViewController {
         foodTextField.keyboardType = .numberPad
     }
     
+    func totalLevelCount() {
+        let totalCount = (foodCount/5) + (driknCount/2)
+        var totalLevel = 0
+        
+        switch totalCount {
+        case 0..<10:
+            totalLevel = 1
+        case 10..<20:
+            totalLevel = 1
+        case 20..<30:
+            totalLevel = 2
+        case 30..<40:
+            totalLevel = 3
+        case 40..<50:
+            totalLevel = 4
+        case 50..<60:
+            totalLevel = 5
+        case 60..<70:
+            totalLevel = 6
+        case 70..<80:
+            totalLevel = 7
+        case 80..<90:
+            totalLevel = 8
+        case 90..<100:
+            totalLevel = 9
+            
+        default:
+            totalLevel = 10
+        }
+        levelText.text = "LV\(totalLevel)"
+    }
+    
     @IBAction func eatButtonClicked(_ sender: UIButton) {
         if foodTextField.text != "" {
             let foodNumber: Int? = Int(foodTextField.text ?? "")
@@ -35,12 +74,21 @@ class GrowingViewController: UIViewController {
             foodCount += 1
         }
         foodLabel.text = "밥알 \(foodCount)개"
+        totalLevelCount()
+
     }
     
     
     @IBAction func drinkButtonClicked(_ sender: UIButton) {
-        driknkCount += 1
-        drinkLabel.text = "물방울 \(driknkCount)개"
+        if drinkTextField.text != "" {
+            let foodNumber: Int? = Int(drinkTextField.text ?? "")
+            driknCount += foodNumber ?? 0
+        } else {
+            driknCount += 1
+            drinkLabel.text = "물방울 \(driknCount)개"
+        }
+        foodLabel.text = "밥알 \(foodCount)개"
+        totalLevelCount()
     }
     
 

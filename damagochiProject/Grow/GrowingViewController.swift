1772110952a8f2db7cc4ea13f8245a084e037973
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-07-23 14:37:36 +0900

    GrowingStroryboard Layout 완성 및 ButtonClick 하면 foodCount 개수 증가 구현

--- damagochiProject/Grow/GrowingViewController.swift
+++ damagochiProject/Grow/GrowingViewController.swift
@@ -8,22 +8,41 @@
 import UIKit
 
 class GrowingViewController: UIViewController {
-
+    
+    @IBOutlet weak var talkLabel: UILabel!
+    @IBOutlet weak var foodLabel: UILabel!
+    
+    @IBOutlet weak var drinkLabel: UILabel!
+    
+    @IBOutlet weak var foodTextField: UITextField!
+    var foodCount = 0
+    var driknkCount = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        foodTextFieldDesign()
+        
+    }
+    
+    func foodTextFieldDesign() {
+        foodTextField.keyboardType = .numberPad
+    }
+    
+    @IBAction func eatButtonClicked(_ sender: UIButton) {
+        if foodTextField.text != "" {
+            let foodNumber: Int? = Int(foodTextField.text ?? "")
+            foodCount += foodNumber ?? 0
+        } else {
+            foodCount += 1
+        }
+        foodLabel.text = "밥알 \(foodCount)개"
+    }
+    
+    
+    @IBAction func drinkButtonClicked(_ sender: UIButton) {
+        driknkCount += 1
+        drinkLabel.text = "물방울 \(driknkCount)개"
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

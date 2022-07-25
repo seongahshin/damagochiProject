66bc3b7efdeaa03cf0ee07c30f39257033f4dadb
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-07-25 20:23:30 +0900

    다마고치 소개글 수정

--- damagochiProject/damaInfo.swift
+++ damagochiProject/damaInfo.swift
@@ -10,9 +10,9 @@ import Foundation
 struct damaInfo {
     
     let dama: [Dama] = [
-        Dama(imageName: "1-6", damaDescribe: "따끔따끔 다마고치", damaDetailDescribe: "저느 따끔따끔 다마고치입니당", damaNumber: 1),
+        Dama(imageName: "1-6", damaDescribe: "따끔따끔 다마고치", damaDetailDescribe: "저는 따끔따끔 다마고치입니당. 이름은 따끔따금 다마고치이지만 성격은 아주 좋아유! 저는 세상에서 노는 걸 가장 좋아한답니다!", damaNumber: 1),
         Dama(imageName: "2-6", damaDescribe: "방실방실 다마고치", damaDetailDescribe: "저는 방실방실 다마고치입니당 키는 100km 몸무게는 150톤이에용 성격은 화끈하고 날라다닙니당~! 열심히 잘 먹고 잘 클 자신은 있습니당 방실방실!", damaNumber: 1),
-        Dama(imageName: "3-6", damaDescribe: "반짝반짝 다마고치", damaDetailDescribe: "저는 반짝반짝 다마고치입니당", damaNumber: 1)
+        Dama(imageName: "3-6", damaDescribe: "반짝반짝 다마고치", damaDetailDescribe: "저는 반짝반짝 다마고치입니당 이름처럼 반짝반짝 빛나는 다마고치이죠! 저는 세상에서 여행하는 걸 가장 좋아해요! 저와 같이 여행 떠나시지 않으실래요?", damaNumber: 1)
     ]
     
 }
